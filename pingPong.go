dfd701a3bba58dbab748bee339420daab4676405
brunin16 <bruedu16@gmail.com>
2025-07-22 15:24:55 -0300

    done

---
+++ pingPong.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func createMessage() <-chan string {
+	message := make(chan string)
+	go func() {
+		for i := 0; ; i++ {
+			if i%2 == 0 {
+				message <- "Ping"
+			} else {
+				message <- "Pong"
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	return message
+}
+
+func printMessage(message <-chan string) {
+	for msg := range message {
+		fmt.Println(msg)
+	}
+}
+
+func main() {
+	go printMessage(createMessage())
+	select {}
+}
